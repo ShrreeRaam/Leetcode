a70cb0ab53cf7c8eb16b831d1b74fb04423abd81
ShrreeRaam <thalaraam894@gmail.com>
2025-07-02 12:12:45 +0530

    Time: 2 ms (65.89%), Space: 42.1 MB (61.33%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.java
+++ 0010-regular-expression-matching/0010-regular-expression-matching.java
@@ -5,26 +5,19 @@ class Solution {
 
         boolean[][] dp = new boolean[m + 1][n + 1];
         dp[0][0] = true;
-
-        // Deals with patterns like a*, a*b*, a*b*c* etc.
         for (int j = 2; j <= n; j++) {
             if (p.charAt(j - 1) == '*') {
                 dp[0][j] = dp[0][j - 2];
             }
         }
-
-        // Fill the DP table
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 char sc = s.charAt(i - 1);
                 char pc = p.charAt(j - 1);
-
                 if (pc == '.' || pc == sc) {
                     dp[i][j] = dp[i - 1][j - 1];
                 } else if (pc == '*') {
-                    // zero occurrence
                     dp[i][j] = dp[i][j - 2];
-                    // one or more if matches preceding character
                     if (p.charAt(j - 2) == '.' || p.charAt(j - 2) == sc) {
                         dp[i][j] |= dp[i - 1][j];
                     }
